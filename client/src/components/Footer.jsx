ad3ff16cf271add79d1d40850179f35dfdef4c0b
SorinC6 <kish.sorin@yahoo.com>
2022-08-29 18:24:47 +0300

    add new logo and connect btn

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-import logo from "../images/logo.png";
+import logo from "../images/ethereum.svg";
 
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-[0.5] justify-center items-center">
-        <img src={logo} alt="logo" className="w-32" />
+        <div className="flex">
+          <img src={logo} alt="logo" className="w-10 cursor-pointer" />
+          <h3 className="mt-2 text-white text-lg ml-2 font-semibold">
+            Send ETH
+          </h3>
+        </div>
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
         <p className="text-white text-base text-center mx-2 cursor-pointer">
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+import { TransactionContext } from "../context/TransactionContext";
 
-import logo from "../images/logo.png";
+import logo from "../images/ethereum.svg";
 
 const NavBarItem = ({ title }) => (
-  <li className="mx-4 cursor-pointer">{title}</li>
+  <li className="mx-4 cursor-pointer invisible">{title}</li>
 );
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const { currentAccount, connectWallet } = useContext(TransactionContext);
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <div className="flex">
+          <img src={logo} alt="logo" className="w-10 cursor-pointer" />
+          <h3 className="mt-2 text-white text-lg ml-2 font-semibold">
+            Send ETH
+          </h3>
+        </div>
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      <ul className="items-start text-white md:flex hidden list-none flex flex-row items-start">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
+        {!currentAccount && (
+          <li
+            className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            onClick={connectWallet}
+          >
+            Connect
+          </li>
+        )}
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
