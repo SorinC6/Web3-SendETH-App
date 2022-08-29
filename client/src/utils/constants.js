1b57251508fab048c69dfdb253871f0c507578a9
SorinC6 <kish.sorin@yahoo.com>
2022-08-29 12:04:05 +0300

    deploy smart contarct and add trasnaction abi

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transaction.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0x5f0b39FE628b2665C2A58B16B6975f2Be7De8F62'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/NXtQDPWtomD9bp-Bqovqvbp_gM-gVOgU',
+      accounts: ['724d16fe783d1bcebe314d8038a322c375f6706ce4afa26d1d8688e04facdaac'],
+    },
+  }
 };
